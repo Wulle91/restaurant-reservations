816543eed07b7974f917a4eeb4dcda7be143c454
Wulle91 <mvuckovi91@gmail.com>
2023-04-19 10:12:14 +0000

    Add Date Table

--- reservations/models.py
+++ reservations/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -7,23 +8,13 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Reservation(models.Model):
     name = models.CharField(max_length=20, null=False, blank=False)
     email = models.EmailField()
-    TABLE1 = "Table1"
-    TABLE2 = "Table2"
-    TABLE3 = "Table3"
-    TABLE4 = "Table4"
-    TABLE5 = "Table5"
-    LIST_OF_TABLES = [
-        (TABLE1, "Table1"),
-        (TABLE2, "Table2"),
-        (TABLE3, "Table3"),
-        (TABLE4, "Table4"),
-        (TABLE5, "Table5"),
-    ]
-    table = models.CharField(
-        max_length=6,
-        choices=LIST_OF_TABLES,
-        default=None,
-    )
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # Validators should be a list
     created_on = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
     staus = models.IntegerField(choices=STATUS, default=0)
+
+
+class Date(models.Model):
+    reservation_date = models.DateTimeField()
+    table = models.ForeignKey(Reservation, on_delete=models.CASCADE)
