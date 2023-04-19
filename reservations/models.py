330eaf478b6de8fb4101f88ee27d673a653a587f
Wulle91 <mvuckovi91@gmail.com>
2023-04-19 09:33:09 +0000

    Adjust model

--- reservations/models.py
+++ reservations/models.py
@@ -7,7 +7,23 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Reservation(models.Model):
     name = models.CharField(max_length=20, null=False, blank=False)
     email = models.EmailField()
-    table = models.TextChoices("table", "table1 table2 table3 table4 table5")
+    TABLE1 = "Table1"
+    TABLE2 = "Table2"
+    TABLE3 = "Table3"
+    TABLE4 = "Table4"
+    TABLE5 = "Table5"
+    LIST_OF_TABLES = [
+        (TABLE1, "Table1"),
+        (TABLE2, "Table2"),
+        (TABLE3, "Table3"),
+        (TABLE4, "Table4"),
+        (TABLE5, "Table5"),
+    ]
+    table = models.CharField(
+        max_length=6,
+        choices=LIST_OF_TABLES,
+        default=None,
+    )
     created_on = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
     staus = models.IntegerField(choices=STATUS, default=0)
