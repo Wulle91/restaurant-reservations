5bdb2a2adddf76a95f79faaca0bd1c91120e3b87
Wulle91 <mvuckovi91@gmail.com>
2023-04-19 00:47:02 +0000

    Add first model

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
 # Create your models here.
+class Reservation(models.Model):
+    name = models.CharField(max_length=20, null=False, blank=False)
+    email = models.EmailField()
+    table = models.TextChoices("table", "table1 table2 table3 table4 table5")
+    created_on = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField()
+    staus = models.IntegerField(choices=STATUS, default=0)
